d4e1502a29b64d64f47c740d934e0f435237758b
ArkenStorm <shadowarchon7@gmail.com>
2022-12-31 23:56:49 -0700

    add git pull command

---
+++ commands/utility/git.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { exec } = require('child_process');
+const { logError } = require('../../utils');
+
+const data = new SlashCommandBuilder()
+	.setName('git')
+	.setDescription('Performs git interactions')
+	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+	.addSubcommand(subcommand =>
+		subcommand.setName('pull')
+			.setDescription('Updates the bot everywhere via the git repository')
+	);
+
+module.exports = {
+	data,
+	async execute(interaction) {
+		if (interaction.user.id !== '358333674514677760') {
+			await interaction.reply({ content: 'Sorry, this command is reserved for the bot owner only', ephemeral: true });
+			return;
+		}
+		await interaction.reply({ content: 'Git process started; Praetor will be online again shortly.', ephemeral: true });
+		exec('git pull && npm install && npm run restart',
+			async (error) => {
+				if (error) {
+					logError(interaction.client, error, interaction);
+				}
+			});
+	}
+};
--- deployCommands.js
+++ deployCommands.js
@@ -5,15 +5,31 @@ const path = require('node:path');
 const guildId = '383889230704803851'; // Arkchat guildId
 // does clientId need to be dynamic with sharding?
 
+const cliArgs = process.argv.slice(2);
+
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = getFilepaths(commandsPath);
-const commands = commandFiles.map(filepath => require(filepath).data.toJSON());
+const allCommands = commandFiles.map(filepath => require(filepath).data.toJSON());
+/**
+ * Example commands:
+ * 	1) command: node deployCommands.js
+ * 		effect: deploys all commands to the Arkchat guild, for testing
+ * 	2) command: node deployCommands.js -g fun/stats.js
+ * 		effect: deploys the `stats` command globally
+ * 	3) command: node deployCommands.js -g --all
+ * 		effect: deploys all commands globally
+ */
+const commands = (!cliArgs.includes('-g') && !cliArgs.includes('--global')) ?
+	allCommands :
+	(cliArgs.includes('--all') ?
+		allCommands :
+		[require(path.join(__dirname, 'commands', cliArgs.find(arg => arg.includes('.js')))).data.toJSON()]
+	);
 
 const rest = new REST({ version: '10' }).setToken(token);
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-		const cliArgs = process.argv.slice(2);
 		const route = (cliArgs.includes('-g') || cliArgs.includes('--global')) ?
 			Routes.applicationCommands(clientId) :
 			Routes.applicationGuildCommands(clientId, guildId);
